d7c1375e67b85b24b9768c214e6f551137bc068f
qmdx00 <ywm00@qq.com>
2018-01-03 13:45:07 +0800

    day-01-03

---
+++ src/com/lzw/view/Login.java
@@ -0,0 +1,60 @@
+package com.lzw.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private static Login frame;
+	private static MainFrame f;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(460, 240, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				showFrame();
+			}
+		});
+		btnNewButton.setBounds(151, 97, 93, 23);
+		contentPane.add(btnNewButton);
+	}
+	public static void showFrame(){
+		f = new MainFrame();
+		f.setVisible(true);
+		frame.setVisible(false);
+		frame = null;
+	}
+}
---
+++ src/com/lzw/view/MainFrame.java
@@ -0,0 +1,26 @@
+package com.lzw.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+	
+	public MainFrame() {
+		setFont(new Font("ËÎÌו", Font.PLAIN, 18));
+		setTitle("\u56FE\u4E66\u7BA1\u7406\u7CFB\u7EDF");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(360, 140, 700, 460);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
